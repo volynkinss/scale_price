d74176211d44886ec3d9424e8439d26b8aef2644
Volynkin Sergey <127946466+volynkinss@users.noreply.github.com>
2023-08-28 17:46:54 +0300

    created another file to store the queryes, edit total msg

---
+++ queryes.py
@@ -0,0 +1,30 @@
+class Queryes:
+    def jetton_name_query(self, address):
+        query = (
+            """
+                query JettonName {
+                        redoubt_jetton_master (where: {address: {_eq:"%s"}}) {
+                            name
+                        }
+                    }
+                """
+            % address
+        )
+        return query
+
+    def dex_swaps_query(self):
+        query = """
+                    query GetDexSwaps {
+                            redoubt_dex_swaps {
+                                msg_id
+                                platform
+                                swap_dst_token
+                                swap_src_token
+                                swap_dst_amount
+                                swap_src_amount
+                                swap_time
+                                swap_user
+                            }
+                        }
+                    """
+        return query
--- swap_transactions.py
+++ swap_transactions.py
@@ -6,6 +6,8 @@ import asyncio
 from for_bot import api_redebout
 from loguru import logger
 from redoubt_agent import RedoubtEventsStream
+from queryes import Queryes
+from decimal import Decimal
 
 
 class GraphqlQuery:
@@ -13,33 +15,41 @@ class GraphqlQuery:
         self.api_key = api_key
         self.stream = RedoubtEventsStream(api_key)
 
-    async def handler(self):
-        swaps_info = await self.stream.execute(
-            """
-            query GetDexSwaps {
-                    redoubt_dex_swaps {
-                        msg_id
-                        platform
-                        swap_dst_token
-                        swap_src_token
-                        swap_dst_amount
-                        swap_src_amount
-                        swap_time
-                        swap_user
-                    }
-                }
-            """
-        )
+    async def get_jetton_name(self, address):
+        query = Queryes()
+        jetton_name_query = await self.stream.execute(query.jetton_name_query(address))
+        if jetton_name_query["redoubt_jetton_master"] == []:
+            name = "UKWN Coin"
+        else:
+            name = jetton_name_query["redoubt_jetton_master"][0]["name"]
+        return name
+
+    async def get_swap_transactions(self):
+        query = Queryes()
+        swaps_info = await self.stream.execute(query.dex_swaps_query())
         swaps_info = swaps_info["redoubt_dex_swaps"]
         if len(swaps_info) == 0:
             logger.info("dex swaps not found")
+            return
         for index in range(len(swaps_info)):
-            swap_info_msg = f"\nSwap id № {swaps_info[index]['msg_id']} at {swaps_info[index]['swap_time']} on {swaps_info[index]['platform']} platform\nby user {swaps_info[index]['swap_user']}:\n{swaps_info[index]['swap_src_amount']} {swaps_info[index]['swap_src_token']} => {swaps_info[index]['swap_dst_amount']} {swaps_info[index]['swap_dst_token']}"
+            swap_operation = swaps_info[index]
+            src_token_name = await self.get_jetton_name(
+                swap_operation["swap_src_token"]
+            )
+            dst_token_name = await self.get_jetton_name(
+                swap_operation["swap_dst_token"]
+            )
+            total_rate = Decimal(swap_operation["swap_src_amount"]) / Decimal(
+                swap_operation["swap_dst_amount"]
+            )
+            swap_info_msg = f"""\nSwap id № {swap_operation['msg_id']} at {swap_operation['swap_time']} on {swap_operation['platform']} platform
+by user {swap_operation['swap_user']}:
+{src_token_name} => {dst_token_name} at a rate of {total_rate}"""
             logger.info(swap_info_msg)
 
     async def start_check(self):
         logger.info("Running dex swaps checker")
-        await self.handler()
+        await self.get_swap_transactions()
 
 
 async def start():
